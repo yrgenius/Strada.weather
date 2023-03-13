3804f215dace40efd74132f82aa745fc191d1859
yrgenius <yrgenius80@gmail.com>
2023-03-13 18:43:06 +0900

    change http on https

--- requestServer.js
+++ requestServer.js
@@ -1,7 +1,7 @@
 import { renderNow } from "./renderNow.js";
 import { store } from "./store.js";
 
-const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
+const serverUrl = 'https://api.openweathermap.org/data/2.5/weather';
 const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
 // json ответа https://openweathermap.org/current#current_JSON 
 
